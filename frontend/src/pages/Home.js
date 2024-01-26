d4fa084288ed22761987c006bccb6316f797ab03
Ruthvik283 <ruthvik92931@gmail.com>
2024-01-26 18:36:02 +0530

    started react app

---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,19 @@
+import React, { useState, useEffect } from 'react';
+import Footer2  from '../components/Footer'
+import Navbar  from '../components/Navbar'
+
+
+export default function Home() {
+
+    return (
+        <>
+
+            <Navbar></Navbar>
+            
+            <div className="bg-slate-300 h-10">Hii guys</div>
+
+            <Footer2></Footer2>
+
+        </>
+    )
+}
