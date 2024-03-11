6d7b667038b6a3f42e7bcc7d2bf2feba59212ed1
Samyak D <samyakd990@gmail.com>
2024-03-12 03:57:00 +0530

    sanction return

--- backend/materials/models.py
+++ backend/materials/models.py
@@ -1,12 +1,16 @@
 from django.db import models
 from django.contrib.auth import get_user_model
+from picklefield.fields import PickledObjectField
+from datetime import datetime
 User = get_user_model()
 
 
 # Create your models here.
 class Category(models.Model):
     name = models.CharField(max_length=128)
-    parent_category = models.ForeignKey('self', on_delete=models.SET_NULL, null=True, blank=True, related_name='subcategories')
+    parent_category = models.ForeignKey(
+        'self', on_delete=models.SET_NULL, null=True, blank=True, related_name='subcategories')
+
     class Meta:
         verbose_name = "Category"
         verbose_name_plural = "Categories"
@@ -14,13 +18,15 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
+
 class Material(models.Model):
-    category = models.ForeignKey(Category, on_delete=models.SET_NULL, related_name='materials',null=True,blank=True)
+    category = models.ForeignKey(
+        Category, on_delete=models.SET_NULL, related_name='materials', null=True, blank=True)
     material_id = models.AutoField(primary_key=True)
     material_name = models.CharField(max_length=255, default="Un-named")
     price = models.IntegerField(null=False)
     quantity = models.IntegerField(null=False)
-    critical_quantity = models.IntegerField(null=False,default=5)
+    critical_quantity = models.IntegerField(null=False, default=5)
     rack_number = models.CharField(max_length=255, null=True, blank=True)
     row_number = models.CharField(max_length=255, null=True, blank=True)
 
@@ -66,6 +72,25 @@ class Sanction(models.Model):
     material = models.ForeignKey(Material, on_delete=models.CASCADE)
     date_time = models.DateTimeField(auto_now=True)
     quantity_sanctioned = models.IntegerField(null=False)
+    log = PickledObjectField(default=list)
+
+    def sanction_return(self, quantity: int):
+        if quantity > self.quantity_sanctioned:
+            return False
+        self.log = self.log + [str(datetime.now()), -quantity]
+        self.quantity_sanctioned -= quantity
+        self.material.quantity += quantity
+        super.save()
+        self.material.save()
+
+    def sanction_add(self, quantity: int):
+        if quantity > self.material.quantity:
+            return False
+        self.log = self.log + [str(datetime.now()), quantity]
+        self.quantity_sanctioned += quantity
+        self.material.quantity -= quantity
+        super.save()
+        self.material.save()
 
     def is_valid(self):
         if (self.quantity_sanctioned <= self.material.quantity):
@@ -74,6 +99,13 @@ class Sanction(models.Model):
 
     def save(self, *args, **kwargs):
         # Call the original save method
+        super().save(*args, **kwargs)
+        self.log = self.log + [
+            [
+                str(self.date_time),
+                int(self.quantity_sanctioned)
+            ]
+        ]
         super().save(*args, **kwargs)
 
         # Update the quantity of the associated material
@@ -84,6 +116,7 @@ class Sanction(models.Model):
 class Role(models.Model):
     # department = models.ForeignKey(Department, on_delete=models.CASCADE)
     role_name = models.CharField(max_length=255, default="Student")
+
     def __str__(self):
         # print(self.material.quantity)
-        return self.role_name   
+        return self.role_name
