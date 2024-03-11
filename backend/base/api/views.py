4fb36e4da38b31e0c3124b7f0ec04a77d4865284
Ruthvik283 <ruthvik92931@gmail.com>
2024-03-11 19:39:43 +0530

    create material n category API

--- backend/base/api/views.py
+++ backend/base/api/views.py
@@ -3,6 +3,7 @@ from rest_framework import serializers, status
 from materials.models import *
 from .serializers import *
 from rest_framework.response import Response
+from rest_framework import generics
 from rest_framework.decorators import api_view
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
@@ -259,3 +260,11 @@ def sanctionsData(request):
     res = SanctionSerializer(Sanction.objects.all(), many=True)
 
     return Response(res.data)
+
+class CategoryCreateView(generics.CreateAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+class MaterialCreateView(generics.CreateAPIView):
+    queryset = Material.objects.all()
+    serializer_class = MaterialSerializer
