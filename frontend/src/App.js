26970842c3966d74363bd792b9c18caa8007a337
Ruthvik283 <ruthvik92931@gmail.com>
2024-03-22 12:04:47 +0530

    fixed modify sanction update issue

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -20,7 +20,7 @@ import { AuthProvider } from "./context/AuthContext";
 import AuthContext from "./context/AuthContext";
 import SignupPage from "./pages/Signup";
 import { Toaster } from "react-hot-toast";
-import ModifySanctionForm from "./components/ModifySanction";
+import ModifySanctionForm from "./pages/ModifySanction";
 
 
 export default function App() {
